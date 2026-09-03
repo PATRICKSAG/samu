257abf346312dbfaa9b79cdecf08f3600bbb480f
Patrick Billy Sagastegui Castillo <sanandreas86@hotmail.com>
2026-09-02 21:12:57 -0500

    feat: Add 'ENVIADO A FISCALIA' option to estadoExpediente selection in formExpedienteUFREMID

--- presentacion/formExpedienteUFREMID.php
+++ presentacion/formExpedienteUFREMID.php
@@ -459,6 +459,7 @@ $tiposActividad     = []; // se cargarán vía AJAX
                                 <option value="CERRADO" <?php echo ($estadoExpediente == 'CERRADO') ? 'selected' : '' ?>>CERRADO</option>
                                 <option value="ARCHIVADO" <?php echo ($estadoExpediente == 'ARCHIVADO') ? 'selected' : '' ?>>ARCHIVADO</option>
                                 <option value="ENVIADO AL EJECUTOR" <?php echo ($estadoExpediente == 'ENVIADO AL EJECUTOR') ? 'selected' : '' ?>>ENVIADO AL EJECUTOR</option>
+                                <option value="ENVIADO A FISCALIA" <?php echo ($estadoExpediente == 'ENVIADO A FISCALIA') ? 'selected' : '' ?>>ENVIADO A FISCALIA</option>
                                 <option value="OTRO" <?php echo ($estadoExpediente == 'OTRO') ? 'selected' : '' ?>>OTRO</option>
                             </select>
                         </div>
